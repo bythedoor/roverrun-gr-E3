4587ad85a35866703f9e06c3163ed55c0a0cb72a
trtony25 <150559400+trtony25@users.noreply.github.com>
2024-11-25 17:50:38 +0100

    Update tree.h
    ajout fonctions : 
    -void FindPathToLeaf(t_node* targetLeaf);
    
    -t_node* findMinLeaf(t_node* root);
    
    -void displayMoves(t_move tab_moves[],int i);

--- tree.h
+++ tree.h
@@ -21,4 +21,10 @@ void swap(int * list, int a); //Swap the first element of the list with another 
 
 int* new_list(int* list, int a, int nb_move_remaining); //Return
 
+t_node* findMinLeaf(t_node* root);    //trouver le noeud avec la val la plus petite
+
+void FindPathToLeaf(t_node* targetLeaf);    //trouver le meilleur chemin  
+
+void displayMoves(t_move tab_moves[],int i);    //afficher les mouvements
+
 #endif //UNTITLED1_TREE_H
